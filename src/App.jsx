6a70029e19af6c05d7f406fdb4a73c4ced213e0e
roxananavahr <navaroxanahs@gmail.com>
2025-04-01 18:37:07 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react"
+import './App.css'
+import AnimalsShow from './AnimalsShow'
+
+function getRandomAnimal(){
+  const animals = ['cat', 'bird', 'dog', 'gastor', 'horse']
+  return animals[Math.floor(Math.random()* animals.length)]
+}
+
+console.log(getRandomAnimal())
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [animals, setAnimals] = useState([])
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderAnimals = animals.map((animal, index) =>{
+    return <AnimalsShow type={animal} key={index}/>
+
+  })
+
 
   return (
-    <>
-   <h1>Animals  App</h1>
-    </>
+    <div className='app'>
+  <button onClick={handleClick}>Add Animal</button>
+  <div className="animals-list">
+  {renderAnimals}
+  </div>
+  </div>
   )
 }
 
